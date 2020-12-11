5fb7541e832d7a0e1e7567ea28305a540f58d30c
mackayeg <46541091+mackayeg@users.noreply.github.com>
2020-12-11 13:44:45 -0600

    cleaned up day 10 part 1

--- 2020/Day11/11a.py
+++ 2020/Day11/11a.py
@@ -1,16 +1,9 @@
+import time
+
 f = open("Day11\\11.txt")
 test = f.read()
 f.close()
-temp = test.splitlines()
-total_rows = len(temp)
-total_columns = len(temp[0])
-seatmap = temp.copy()
-'''
-for y in range(total_rows):
-    for x in range(total_columns):
-        seats.append(d(temp[y][x])
-
-print(seats)'''
+seatmap = test.splitlines()
 
 occupied = "#"
 floor = "."
@@ -20,28 +13,26 @@ empty = "L"
 # If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
 # Otherwise, the seat's state does not change.
 
-def is_occupied(r, c, s):
+def is_occupied(r, c, s): #checks to see how many adjacent seats are occupied
     # y = row
     # x = column
     starty = -1
     startx = -1
     full = 0
+    #this is a messy way to deal with negative indexes but meh
     if r == 0: starty = 0
     if c == 0: startx = 0
+    #loop through 8 adjacent cells plus 0,0 - probably a better way to do this
     for x in range(startx, 2):
         for y in range(starty, 2):
-            try:
+            try: #if index it out of range deal with it
                 if(s[y+r][x+c] == occupied): full += 1
             except:
-                continue#print("poop")
+                continue #on the edge, don't care
     return full
 
-row = 0
-column = 0
-#for rows, for columns, etc
 full_counter = 0
 full_counter_old = -1
-
 
 def tea_party(seats):
     global full_counter
@@ -49,31 +40,25 @@ def tea_party(seats):
 
     full_counter_old = full_counter
 
-    seats_old = seats.copy()
+    old_seating = seats.copy()
     for row in range(len(seats)):
         for column in range(len(seats[row])):
             #this checks for seat adjacency and returns the number of full, adjacent seats
-            adjacent = is_occupied(row,column, seats_old)
-            if (seats_old[row][column] == empty and (adjacent == 0)):
-                    seats[row] = seats[row][:column] + occupied + seats[row][column+1:]
-                    full_counter += 1
-                    #print("Sat down!")
-            elif (seats_old[row][column] == occupied and (adjacent > 4)): #4 is the limit but we always count ourselves
+            adjacent = is_occupied(row,column, old_seating)
+            if (old_seating[row][column] == empty and (adjacent == 0)):
+                #Seat is empty and there are no people around, sit on down!
+                seats[row] = seats[row][:column] + occupied + seats[row][column+1:]
+                full_counter += 1
+            elif (old_seating[row][column] == occupied and (adjacent > 4)): #4 is the limit but we always count ourselves
+                #Too many people around!  Time to leave!
                 seats[row] = seats[row][:column] + empty + seats[row][column+1:]
-                #print("left!")
                 full_counter += -1    
-    return seats #return the new list!
+    return seats #return the new list
 
-while full_counter != full_counter_old:
-    seatmap = tea_party(seatmap)
-    test = 0
-    '''for x in range(len(seats)): test += seats[x].count(occupied)
-    if test != full_counter:
-        print("PROBLEM!")
-        break'''
+#Keep changing seats until we settle
+start_time = time.time()
+while full_counter != full_counter_old: seatmap = tea_party(seatmap)
 
-
-
-
-print(full_counter)
-print(seatmap)
+print("\nThe number of occupied seats is: " + str(full_counter))
+print("Execution took %s seconds\n" % (time.time() - start_time))
+#EOF
