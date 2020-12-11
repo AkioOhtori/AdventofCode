a06045414965408e43976a51fcccf35fd41f3460
mackayeg <46541091+mackayeg@users.noreply.github.com>
2020-12-11 09:51:15 -0600

    Right answer

--- 2020/Day10/10b-2.py
+++ 2020/Day10/10b-2.py
@@ -1,37 +1,40 @@
 # Adapter rating = +0j, - 1-3j
 # Build in adatper = highest + 3j
 # Outlet = 0j
-name = "Day10\\10as1.txt"
+name = "Day10\\10.txt"
 f = open(name)
 test = f.read().splitlines()
 f.close()
-adapters = [0] #0 for outlet
+adapters = list()#[0] #0 for outlet
+adapters.append(0)
 arragements = 0
 iterations = 0
 
 for a in range(len(test)): adapters.append(int(test[a].strip()))
 
-adapters.append(adapters[len(adapters)-1] + 3) #append internal adapter
 adapters.sort(reverse=True) #sort smallest to largest
+adapters.insert(0, adapters[0] + 3) #append internal adapter
 end = len(adapters)-1
 
 ways_to = dict()
+#print(adapters)
 
 for x in adapters: ways_to[x] = 0
 
-for x in adapters:
-    print(x)
-    y = 0
-    for n in range(0,4):
+ways_to[adapters[0]] = 1
+
+for x in range(len(adapters)):
+    y = ways_to[adapters[x]]
+    print("Adapter " + str(adapters[x]) + ", dict: " + str(ways_to[adapters[x]]))
+    for n in range(1,4):
         if (x + n) <= end:
             if (adapters[x] - adapters[x+n]) <= 3:
-                y += 1
-    ways_to[x] += y
+                ways_to[adapters[x+n]] += y
 
-answer = 0
-for x in adapters:
-    answer = answer+ways_to[x]
-print(ways_to)
-print(answer)
+#answer = 0
+#for x in adapters:
+#    answer = answer+ways_to[x]
+print("\nThere are " + str(ways_to[0]) + " combinations that will work.\n")
+#print(answer)
 
 #EOF
