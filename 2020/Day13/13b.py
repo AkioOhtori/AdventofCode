8fb234ed33b7477cc96da31021720294cb9d905b
mackayeg <46541091+mackayeg@users.noreply.github.com>
2020-12-14 21:52:17 -0600

    un giving up on day 13 part 2

--- 2020/Day13/13b.py
+++ 2020/Day13/13b.py
@@ -1,13 +1,10 @@
 # Parse input, stripping time and "x,"
-floc = "Day13\\13s.txt"
+floc = "Day13\\13.txt"
 f = open(floc)
 
 arrive = int(f.readline().strip())
-
 buses = f.readline().strip().split(",")
-
-alltimes = list()
-print(buses)
+f.close()
 
 test = []
 z = 0
@@ -18,31 +15,17 @@ for bus in buses:
 print(test)
 bus = test
 
-print(bus[-3])
+def modsearch(n, t):
+    if ((t+bus[n][1])%(bus[n][0]) == 0): return 1
+    else: return 0
 
-def search(b, n):
-    #for p in range(len(test)):
-    if b > 1:#len(bus):
-        print("FINISHED")
-        print(b)
-    a1 = 0
-    a2 = 0
-    p = 1
-    while a2 <= a1:
-        a1 = bus[b][0]*n + bus[b][1]
-        a2 = bus[b+1][0]*p + bus[b+1][1]
-
-        if (a1 == a2):
-            #found a hit, move to next sequence
-#            print("HIT")
-            search(b+1, p)
-        else: p += 1
-
-
-
-t=1
-while t < 1068797:
-    search(-3, t)
-    t += 1
+t = 100000000000000
+x = 0
+while x < len(bus):
+    if modsearch(x, t):
+        x+=1
+    else:
+        x = 0
+        t+=1
 
 print(t)
