bae8983ad188588f9fbe00cbfb778ff4739ac116
mackayeg <46541091+em@users.noreply.github.com>
2020-12-15 08:59:38 -0600

    trying and failing to make day 13b faster

--- 2020/Day13/13b.py
+++ 2020/Day13/13b.py
@@ -14,18 +14,19 @@ for bus in buses:
     z += 1
 print(test)
 bus = test
-
+#print(bus[-3])
 def modsearch(n, t):
     if ((t+bus[n][1])%(bus[n][0]) == 0): return 1
     else: return 0
-
+start = -7
 t = 100000000000000
-x = 0
-while x < len(bus):
+x = start
+print(bus[start])
+while x < (len(bus)+start):
     if modsearch(x, t):
         x+=1
     else:
-        x = 0
+        x = start
         t+=1
 
 print(t)
