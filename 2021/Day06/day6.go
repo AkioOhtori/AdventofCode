0810b9fd37d88e75310905c90f36a00cd6ec8964
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-07 11:40:19 -0600

    day 6 part 1 working

--- 2021/Day06/day6.go
+++ 2021/Day06/day6.go
@@ -15,7 +15,7 @@ Second: iterating though to create bit mask
 Third: don't fuck it up
 */
 
-var path = "sample.txt" //path to problem input
+var path = "input.txt" //path to problem input
 const PART int = 1
 
 // Function to handle errors
@@ -24,15 +24,6 @@ func isError(err error) bool {
 		fmt.Println(err.Error())
 	}
 	return (err != nil)
-}
-
-func convTxtSlicetoInt(arr []string) []int {
-	var output []int
-	for _, txt := range arr {
-		val, _ := strconv.Atoi(txt)
-		output = append(output, val)
-	}
-	return output
 }
 
 func main() {
@@ -57,60 +48,27 @@ func main() {
 		input_int = append(input_int, tmp)
 	}
 
-	var fishies [18]int
-	var buffer [8]int
+	var fishies [7]int
+	var fry [9]int
 
 	for _, fish := range input_int {
 		fishies[fish]++
-		// switch fish {
-		// case 0:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 1:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 2:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 3:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 4:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 5:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 6:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 7:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// case 8:
-		// 	fmt.Println(fish)
-		// 	fishies[n]++
-		// }
 	}
 
 	for day := 0; day < 80; day++ {
-		if day < 7 {
-			fishies[day+8] += fishies[day]
-		} else {
-			cycle := day % 6
-			fishies[cycle] += fishies[cycle+6]
-			fishies[cycle+6] = 0
-			fishies[cycle+8] += fishies[cycle]
-
-		}
-		fmt.Printf("%v\t%v\n", day, fishies)
+		cycle := day % 7
+		puberty := day % 9
+		fishies[cycle] += fry[puberty]
+		fry[puberty] = fishies[cycle]
+		// fmt.Printf("%v\t%v\t%v\t%v\n", day, fishies, fry, puberty)
 	}
 	var total int = 0
 	for _, fish := range fishies {
 		total += fish
 	}
-
-	fmt.Println(fishies)
+	for _, fish := range fry {
+		total += fish
+	}
 
 	fmt.Println(total)
 }
