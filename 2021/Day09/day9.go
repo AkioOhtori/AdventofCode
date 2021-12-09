6ab786aa7af1bd96ea75b4e61863b4f805552aeb
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-09 11:36:19 -0600

    day 9 parts 1 and 2 done
    Some cleanup needed

--- 2021/Day09/day9.go
+++ 2021/Day09/day9.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-var path = "sample.txt" //path to problem input
+var path = "input.txt" //path to problem input
 const PART int = 1
 
 // Function to handle errors
@@ -21,6 +22,7 @@ func isError(err error) bool {
 
 func fuckingFuck(i [][]int, x int, y int, a [][2]int) [][2]int {
 	val := i[y][x]
+	// fmt.Printf("Checking %v,%v = %v\n", x, y, i[y][x])
 	if val == 9 {
 		return a
 	}
@@ -28,41 +30,48 @@ func fuckingFuck(i [][]int, x int, y int, a [][2]int) [][2]int {
 	//Do xr (x + 1) first
 	xr := x + 1
 	if xr < len(i[y]) { //make sure it is in bounds, if not, skip
-		if i[y][xr] > val && i[y][xr] != 9 {
+		if i[y][xr] != 9 {
+			// fmt.Printf("I'm on xr and looking at %v,%v = %v\n", y, xr, i[y][xr])
 			if checkAnswer(a, [2]int{y, xr}) {
 				a = append(a, [2]int{y, xr})
 				a = fuckingFuck(i, xr, y, a)
+				// fmt.Printf("Adding %v,%v = %v\n", xr, y, i[y][x])
 			}
 		}
 	}
 	yd := y + 1
 	if yd < len(i) {
-		if i[yd][x] > val && i[yd][x] != 9 {
+		if i[yd][x] != 9 {
+			// fmt.Printf("I'm on yd and looking at %v,%v = %v\n", yd, x, i[yd][x])
 			if checkAnswer(a, [2]int{yd, x}) {
 				a = append(a, [2]int{yd, x})
 				a = fuckingFuck(i, x, yd, a)
+				// fmt.Printf("Adding %v,%v = %v\n", x, yd, i[y][x])
 			}
 		}
 	}
 	xl := x - 1
 	if xl >= 0 {
-		if i[y][xl] > val && i[y][xl] != 9 {
-			if checkAnswer(a, [2]int{y, xr}) {
-				a = append(a, [2]int{y, xr})
+		if i[y][xl] != 9 {
+			// fmt.Printf("I'm on xl and looking at %v,%v = %v\n", y, xl, i[y][xl])
+			if checkAnswer(a, [2]int{y, xl}) {
+				a = append(a, [2]int{y, xl})
 				a = fuckingFuck(i, xl, y, a)
+				// fmt.Printf("Adding %v,%v = %v\n", xl, y, i[y][x])
 			}
 		}
 	}
 	yu := y - 1
 	if yu >= 0 {
-		if i[yu][x] > val && i[y][xl] != 9 {
+		if i[yu][x] != 9 {
+			// fmt.Printf("I'm on yu and looking at %v,%v = %v\n", yu, x, i[yu][x])
 			if checkAnswer(a, [2]int{yu, x}) {
 				a = append(a, [2]int{yu, x})
 				a = fuckingFuck(i, x, yu, a)
+				// fmt.Printf("Adding %v,%v = %v\n", x, yu, i[y][x])
 			}
 		}
 	}
-
 	return a
 }
 
@@ -72,6 +81,7 @@ func checkAnswer(a [][2]int, new [2]int) bool {
 			return false
 		}
 	}
+	// fmt.Printf("Adding %v\n", new)
 	return true
 }
 
@@ -87,8 +97,8 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 	var input_int [][]int
 	var ans_pt1 []int
-	var ans_pt2 [][2]int
-	var omgwtf []int
+	var basin [][2]int
+	var basinSizes []int
 
 	//Go through the instructions and make them usable
 	for scanner.Scan() {
@@ -100,7 +110,7 @@ func main() {
 		}
 		input_int = append(input_int, temp)
 	}
-
+out:
 	for y := 0; y < len(input_int); y++ {
 		for x := 0; x < len(input_int[y]); x++ {
 			//need x+1, x-1, y+1, y-1
@@ -126,11 +136,17 @@ func main() {
 			if val < input_int[yu][x] && val < input_int[yd][x] {
 				if val < input_int[y][xl] && val < input_int[y][xr] {
 					ans_pt1 = append(ans_pt1, val) //add to ans_pt1 array
-					ans_pt2 = fuckingFuck(input_int, x, y, ans_pt2)
-					omgwtf = append(omgwtf, len(ans_pt2))
-					ans_pt2 = [][2]int{}
+					basin = append(basin, [2]int{y, x})
+					// fmt.Printf("Found and added y=%v,x=%v = %v\n", y, x, input_int[y][x])
+					basin = fuckingFuck(input_int, x, y, basin)
+					basinSizes = append(basinSizes, len(basin))
+					basin = [][2]int{}
+					if false {
+						break out
+					}
 				}
 			} //end ifs
+
 		} //end x for loop
 	} // end y for loop
 
@@ -140,10 +156,10 @@ func main() {
 	for _, a := range ans_pt1 {
 		answer += a + 1
 	}
+	sort.Sort(sort.Reverse(sort.IntSlice(basinSizes)))
+	ans_part2 := basinSizes[0] * basinSizes[1] * basinSizes[2]
 
 	fmt.Printf("The answer to Part 1 is %v danger units\n", answer)
-
-	fmt.Println(len(ans_pt2))
-	fmt.Println(omgwtf)
+	fmt.Printf("The answer to Part 2 is %v, apparently\n", ans_part2)
 
 }
