8d301d978274cfac0a7bf783aacadcc25d4902cf
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-09 11:52:02 -0600

    day 9 parts 1 and 2 complete and cleaned

--- 2021/Day09/day9.go
+++ 2021/Day09/day9.go
@@ -20,68 +20,62 @@ func isError(err error) bool {
 	return (err != nil)
 }
 
-func fuckingFuck(i [][]int, x int, y int, a [][2]int) [][2]int {
+//Recurisive function to, essentially, check for 9s and add non-nines to the basin
+//Returns the coordinants of all parts of the basin explored so far (b)
+func exploreBasin(i [][]int, x int, y int, b [][2]int) [][2]int {
 	val := i[y][x]
-	// fmt.Printf("Checking %v,%v = %v\n", x, y, i[y][x])
 	if val == 9 {
-		return a
+		return b
 	}
 
-	//Do xr (x + 1) first
+	//Check one to the left, right, up, and down to see if they're 9
+	//If they're not, add them to the list (if they aren't already there) and explore
 	xr := x + 1
 	if xr < len(i[y]) { //make sure it is in bounds, if not, skip
 		if i[y][xr] != 9 {
-			// fmt.Printf("I'm on xr and looking at %v,%v = %v\n", y, xr, i[y][xr])
-			if checkAnswer(a, [2]int{y, xr}) {
-				a = append(a, [2]int{y, xr})
-				a = fuckingFuck(i, xr, y, a)
-				// fmt.Printf("Adding %v,%v = %v\n", xr, y, i[y][x])
+			if checkAnswer(b, [2]int{y, xr}) {
+				b = append(b, [2]int{y, xr})
+				b = exploreBasin(i, xr, y, b)
 			}
 		}
 	}
 	yd := y + 1
 	if yd < len(i) {
 		if i[yd][x] != 9 {
-			// fmt.Printf("I'm on yd and looking at %v,%v = %v\n", yd, x, i[yd][x])
-			if checkAnswer(a, [2]int{yd, x}) {
-				a = append(a, [2]int{yd, x})
-				a = fuckingFuck(i, x, yd, a)
-				// fmt.Printf("Adding %v,%v = %v\n", x, yd, i[y][x])
+			if checkAnswer(b, [2]int{yd, x}) {
+				b = append(b, [2]int{yd, x})
+				b = exploreBasin(i, x, yd, b)
 			}
 		}
 	}
 	xl := x - 1
 	if xl >= 0 {
 		if i[y][xl] != 9 {
-			// fmt.Printf("I'm on xl and looking at %v,%v = %v\n", y, xl, i[y][xl])
-			if checkAnswer(a, [2]int{y, xl}) {
-				a = append(a, [2]int{y, xl})
-				a = fuckingFuck(i, xl, y, a)
-				// fmt.Printf("Adding %v,%v = %v\n", xl, y, i[y][x])
+			if checkAnswer(b, [2]int{y, xl}) {
+				b = append(b, [2]int{y, xl})
+				b = exploreBasin(i, xl, y, b)
 			}
 		}
 	}
 	yu := y - 1
 	if yu >= 0 {
 		if i[yu][x] != 9 {
-			// fmt.Printf("I'm on yu and looking at %v,%v = %v\n", yu, x, i[yu][x])
-			if checkAnswer(a, [2]int{yu, x}) {
-				a = append(a, [2]int{yu, x})
-				a = fuckingFuck(i, x, yu, a)
-				// fmt.Printf("Adding %v,%v = %v\n", x, yu, i[y][x])
+			if checkAnswer(b, [2]int{yu, x}) {
+				b = append(b, [2]int{yu, x})
+				b = exploreBasin(i, x, yu, b)
 			}
 		}
 	}
-	return a
+	return b //send back the expored basin so far
 }
 
+//Checks new basin coord to make sure we don't alreay have them
 func checkAnswer(a [][2]int, new [2]int) bool {
 	for _, i := range a {
 		if i == new {
 			return false
 		}
 	}
-	// fmt.Printf("Adding %v\n", new)
 	return true
 }
 
@@ -100,7 +94,7 @@ func main() {
 	var basin [][2]int
 	var basinSizes []int
 
-	//Go through the instructions and make them usable
+	//Go through the instructions and convert them to slices of ints
 	for scanner.Scan() {
 		var temp []int
 		input_str := strings.Split(scanner.Text(), "")
@@ -110,7 +104,8 @@ func main() {
 		}
 		input_int = append(input_int, temp)
 	}
-out:
+
+	//Time to explore! (Nested for loops to example entire x by y matrix)
 	for y := 0; y < len(input_int); y++ {
 		for x := 0; x < len(input_int[y]); x++ {
 			//need x+1, x-1, y+1, y-1
@@ -119,6 +114,7 @@ out:
 			xr := x + 1
 			yu := y - 1
 			yd := y + 1
+
 			//if index is out of bounds, set it to the other one so checks still work
 			if xr >= len(input_int[y]) {
 				xr = xl
@@ -136,30 +132,28 @@ out:
 			if val < input_int[yu][x] && val < input_int[yd][x] {
 				if val < input_int[y][xl] && val < input_int[y][xr] {
 					ans_pt1 = append(ans_pt1, val) //add to ans_pt1 array
-					basin = append(basin, [2]int{y, x})
-					// fmt.Printf("Found and added y=%v,x=%v = %v\n", y, x, input_int[y][x])
-					basin = fuckingFuck(input_int, x, y, basin)
-					basinSizes = append(basinSizes, len(basin))
-					basin = [][2]int{}
-					if false {
-						break out
-					}
+
+					//Then deal with Part 2 stuff
+					basin = append(basin, [2]int{y, x})          //add center of basin to basin
+					basin = exploreBasin(input_int, x, y, basin) //explore the basin
+					basinSizes = append(basinSizes, len(basin))  //add the size of the basin to the answers list
+					basin = [][2]int{}                           //clear the basin for next checks
 				}
 			} //end ifs
-
 		} //end x for loop
 	} // end y for loop
 
-	//could have done this inline but heh
+	//Part 1 Answer (could have done this inline but heh)
 	var answer int = 0
-	// var answer2 int = 0
 	for _, a := range ans_pt1 {
 		answer += a + 1
 	}
-	sort.Sort(sort.Reverse(sort.IntSlice(basinSizes)))
+	fmt.Printf("The answer to Part 1 is %v danger units\n", answer)
+
+	//Part 2 Answer Calculation
+	sort.Sort(sort.Reverse(sort.IntSlice(basinSizes))) //Sort largest to smol
 	ans_part2 := basinSizes[0] * basinSizes[1] * basinSizes[2]
 
-	fmt.Printf("The answer to Part 1 is %v danger units\n", answer)
 	fmt.Printf("The answer to Part 2 is %v, apparently\n", ans_part2)
 
 }
