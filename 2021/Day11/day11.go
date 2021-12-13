54af49531fa95751a2cde053e197563ff0563c23
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-13 11:33:53 -0600

    Day 11 part 1 done

---
+++ 2021/Day11/day11.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var path = "input.txt" //path to problem input
+
+// Function to handle errors
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return (err != nil)
+}
+
+func incrementOctopuses(octo [][]int) (tens [][2]int) {
+	// var tens [][2]int
+	for y := 0; y < len(octo); y++ {
+		for x := 0; x < len(octo[y]); x++ {
+			octo[y][x]++
+			if octo[y][x] > 9 {
+				tens = append(tens, [2]int{y, x})
+			}
+		}
+	}
+	return
+}
+
+func incrementAdjacent(o [][]int, n [][2]int, y int, x int) [][2]int {
+	length := len(o)
+	var incrementMap [][2]int
+
+	//Generates a list of valid coordinates for increment
+	for yy := -1; yy <= 1; yy++ {
+		for xx := -1; xx <= 1; xx++ {
+			xxx := xx + x
+			yyy := yy + y
+			if xx == 0 && yy == 0 {
+				continue
+			} else if xxx == length || yyy == length {
+				continue
+			} else if xxx == -1 || yyy == -1 {
+				continue
+			} else {
+				incrementMap = append(incrementMap, [2]int{yyy, xxx})
+			}
+		}
+	}
+	//Iterates over the previously created coordinates and increments
+	for _, yx := range incrementMap {
+		o[yx[0]][yx[1]]++
+		if o[yx[0]][yx[1]] == 10 {
+			if !(checkNew(n, [2]int{yx[0], yx[1]})) {
+				n = append(n, [2]int{yx[0], yx[1]})
+			}
+		}
+	}
+	return n //Return the new "tens" matrix
+}
+
+// Function to better print matrices
+func prettyPrintSlice(m [][]int) {
+	fmt.Printf("\n")
+	for _, y := range m {
+		fmt.Println(y)
+	}
+	fmt.Printf("\n")
+}
+
+//Checks new coord to make sure we don't already have them
+func checkNew(a [][2]int, new [2]int) bool {
+	for _, i := range a {
+		if i == new {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	//Open input file
+	var file, err = os.Open(path)
+	if isError(err) {
+		return
+	}
+
+	//Load all lines in file into a slice of slices
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var input_int [][]int
+
+	//Go through the instructions and convert them to slices of ints, [y],[x]
+	for scanner.Scan() {
+		var temp []int
+		input_str := strings.Split(scanner.Text(), "")
+		for _, x := range input_str {
+			y, _ := strconv.Atoi(x)
+			temp = append(temp, y)
+		}
+		input_int = append(input_int, temp)
+	}
+
+	/*
+		From the prompt, every day 3 things happen:
+		1) Every octopus gets a +1
+		2) Anything with an energy of 9 flashes and adds 1 to adjacent
+		3) Anything that flashed goes to 0
+	*/
+	var answer_pt1 int = 0
+
+	prettyPrintSlice(input_int)
+	for iterations := 0; iterations < 100; iterations++ {
+
+		// Step 1 - Increment all and record any flashes
+		tens := incrementOctopuses(input_int)
+		// fmt.Printf("*** Step %v ***\n", iterations+1)
+		// fmt.Printf("After inc, but before math: %v\n", tens)
+
+		// Step 2 - Increment everything around the flashes AND process new ones
+		for i := 0; i < len(tens); i++ {
+			tens = incrementAdjacent(input_int, tens, tens[i][0], tens[i][1])
+		}
+		answer_pt1 += len(tens)
+		// Step 3 - Zero out anything that flashed, regardless of current value
+		for _, flash := range tens {
+			input_int[flash[0]][flash[1]] = 0
+		}
+
+		// fmt.Printf("After math: %v\n", tens)
+		// prettyPrintSlice(input_int)
+
+	}
+	fmt.Printf("The answer to part 1, how many flashes after 100 cycles, is %v\n", answer_pt1)
+
+}
--- testing/hello.go
+++ testing/hello.go
@@ -1,13 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func inc(sli []int, n int) []int {
+	sli = append(sli, n+1)
+	return sli
+}
 
 func main() {
-	x := []string{"a", "b", "c"}
-	fmt.Println(len(x))
-	x = append(x[1:])
-	fmt.Println(len(x))
-	fmt.Println((x))
+	x := []int{1, 2, 3}
+
+	// for _, poop := range x {
+	// 	x = inc(x, poop)
+	// 	fmt.Println(x)
+	// }
+	moist := len(x)
+	for z := 0; z < len(x); z++ {
+		x = inc(x, 5)
+		moist = len(x)
+		fmt.Println(x)
+		if moist == 10 {
+			break
+		}
+	}
 }
