835201892ed6bccf6fe8123eafb97f9b6ed8f72e
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-13 17:46:49 -0600

    day 12 cleanup

--- 2021/Day12/day12.go
+++ 2021/Day12/day12.go
@@ -24,12 +24,6 @@ func isError(err error) bool {
 	return (err != nil)
 }
 
-func prettyPrintMatrix(m [][]string) {
-	for _, x := range m {
-		fmt.Println(x)
-	}
-}
-
 func checkLower(p string) bool {
 	for _, x := range p {
 		if unicode.IsLower(x) {
@@ -52,57 +46,50 @@ func checkFor(p []string, new string) bool {
 	return false
 }
 
-func checkDoubleFor(p []string, new string) bool {
-	// var double int = 0
-	// var exists bool = false
+func checkForDouble(p []string) bool {
+	//This function checks to see if ANY double small cave exists
 	for _, l := range lowers {
 		var i int = 0
 		for _, x := range p {
 			if l == x {
-				// exists = true
 				i++
 				if i > 1 {
 					return true
-					// double++
-					// if x == new {
-					// 	return true
-					// } else if double > 1 {
-					// 	return true
-					// }
 				}
 			}
 		}
-		// if exists && double > 0 {
-		// 	return true
-		// }
 	}
-	// fmt.Printf("Did not find %v in p = %v\n", new, p)
 	return false
 }
 
-func traverseTree(tree [][]string, p []string, begin string) {
-	p = append(p, begin)
+// This is the main recursive function that travels the tree starting at START+1
+func traverseTree(tree [][]string, p []string, origin string) {
 
+	p = append(p, origin) //Record where we came from
+
+	//look through the tree to see if we can find a matching path
 	for _, branch := range tree {
-		if branch[ORIGIN] == begin {
-			// fmt.Printf("Found a path from %v to %v in %v\n", begin, branch[DESTIN], branch)
-			if branch[DESTIN] == "end" {
-				// p = append(p, branch[DESTIN])
+		if branch[ORIGIN] == origin { //match found!
+			if branch[DESTIN] == "end" { //if it is an end, record the answer
 				answer = append(answer, p)
 				continue
-			} else if checkLower(branch[DESTIN]) {
-				if checkFor(p, branch[DESTIN]) {
-					if PART == 1 {
+			} else if checkLower(branch[DESTIN]) { //check if lower case
+				if checkFor(p, branch[DESTIN]) { //check if already exists in answer
+					if PART == 1 { //part 1 doesn't allow lowercase duplicates
 						continue
-					} else if checkDoubleFor(p, branch[DESTIN]) {
+					} else if checkForDouble(p) { //part 2 allows exactly 1 lower case duplicate
+						//since we can only get here by having at least 1 of DESTIN
+						//if ANY double also exists, we basically treat it like Part 1
 						continue
 					}
 				}
 			}
-			// p = append(p, branch[DESTIN])
-			// fmt.Printf("Started at %v and going to %v\n", begin, branch[DESTIN])
+
+			//we're going another level deep, but want to keep this p separate, so make a new one
 			new := make([]string, len(p))
 			copy(new, p)
+
+			//Going down another fork!
 			traverseTree(tree, new, branch[DESTIN])
 		}
 	}
@@ -118,13 +105,13 @@ func main() {
 	//Load all lines in file into a slice of slices
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	var input [][]string
-	var starts [][]string
+	var input [][]string  //input minus starting conditions
+	var starts [][]string //starting conditions
 
 	//Go through the instructions and convert them to slices of slices, [ORIGIN],[DESTIN]
 	for scanner.Scan() {
 		input_str := strings.Split(scanner.Text(), "-")
-		// fmt.Println(input_str)
+
 		if input_str[ORIGIN] == "start" {
 			starts = append(starts, input_str)
 		} else if input_str[DESTIN] == "start" {
@@ -136,7 +123,7 @@ func main() {
 				input = append(input, []string{input_str[1], input_str[0]})
 			}
 		}
-
+		//For part 2 we need to know all the "small" caves (lower case)
 		if PART == 2 {
 			if checkLower(input_str[0]) {
 				lowers = append(lowers, input_str[0])
@@ -147,9 +134,10 @@ func main() {
 		}
 	}
 
+	//traverse the tree for all starting conditions
 	for _, start := range starts {
 		traverseTree(input, []string{}, start[DESTIN])
 	}
-	// prettyPrintMatrix(answer)
+
 	fmt.Printf("The answer to Part %v is %v \n", PART, len(answer))
 }
