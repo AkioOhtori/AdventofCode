decbbd109a0d154a1c512619cb73720335f87737
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-14 16:52:11 -0600

    day 13 parts 1 and 2 solved

--- 2021/Day13/day13.go
+++ 2021/Day13/day13.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-var path = "inputG.txt" //path to problem input
+var path = "input.txt" //path to problem input
 const PART int = 1
 
 // Function to handle errors
@@ -57,7 +57,7 @@ func main() {
 
 	var input_int [][]int
 	var fold_instructions [][]string
-	// var paper [][]int
+	var answer_pt1 int = 0
 	var max_x int
 	var max_y int
 
@@ -67,29 +67,58 @@ func main() {
 		if len(temp_str) > 0 {
 			if temp_str[:1] != "f" {
 				input_str := strings.Split(temp_str, ",")
-				// fmt.Println(input_str)
+
 				var temp_int []int
 				for _, x := range input_str {
 					y, _ := strconv.Atoi(x)
 					temp_int = append(temp_int, y)
 				}
 				input_int = append(input_int, temp_int)
-
-				if temp_int[0] > max_x {
-					max_x = temp_int[0]
-				}
-				if temp_int[1] > max_y {
-					max_y = temp_int[1]
-				}
-
 			} else {
 				t := strings.Split(temp_str, "=")
 				fold_instructions = append(fold_instructions, []string{t[0][11:], t[1]})
 			}
 		}
-
 	}
-	//create empty field of correct size
+
+	for _, f := range fold_instructions {
+		if f[0] == "y" && max_y == 0 {
+			max_y, _ = strconv.Atoi(f[1])
+			max_y = max_y * 2
+		} else if f[0] == "x" && max_x == 0 {
+			max_x, _ = strconv.Atoi(f[1])
+			max_x = max_x * 2
+		}
+		if max_x != 0 && max_y != 0 {
+			break
+		}
+	}
+	fmt.Printf("I think we're going to make the matrix x = %v, y = %v\n", max_x, max_y)
+
+	/*
+
+		                                   ,----,            ,---,
+		         ,--.    ,----..         ,/   .`|         ,`--.' |
+		       ,--.'|   /   /   \      ,`   .'  :   ,---,.|   :  :
+		   ,--,:  : |  /   .     :   ;    ;     / ,'  .' |'   '  ;
+		,`--.'`|  ' : .   /   ;.  \.'___,/    ,',---.'   ||   |  |
+		|   :  :  | |.   ;   /  ` ;|    :     | |   |   .''   :  ;
+		:   |   \ | :;   |  ; \ ; |;    |.';  ; :   :  |-,|   |  '
+		|   : '  '; ||   :  | ; | '`----'  |  | :   |  ;/|'   :  |
+		'   ' ;.    ;.   |  ' ' ' :    '   :  ; |   :   .';   |  ;
+		|   | | \   |'   ;  \; /  |    |   |  ' |   |  |-,`---'. |
+		'   : |  ; .' \   \  ',  /     '   :  | '   :  ;/| `--..`;
+		|   | '`--'    ;   :    /      ;   |.'  |   |    \.--,_
+		'   : |         \   \ .'       '---'    |   :   .'|    |`.
+		;   |.'          `---`                  |   | ,'  `-- -`, ;
+		'---'                                   `----'      '---`"
+
+		Apparently not all inputs are created equal and I got the short straw on this one.
+		The above code determines the "paper" size based on the fold directions, rather than the dot locations.
+		This was discovered after I couldn't get the code to work with my input, but it worked great with a friend's.
+		Yes.  I am bitter.  This is why I made this over the top note.
+	*/
+	//Make the "paper" based on max size + 1 (zero ref)
 	paper := make([][]int, max_y+1)
 	for i := 0; i < len(paper); i++ {
 		paper[i] = make([]int, max_x+1)
@@ -97,73 +126,57 @@ func main() {
 	for _, dot := range input_int {
 		paper[dot[1]][dot[0]] = 1
 	}
-	// count := 0
-	// for _, y := range paper {
-	// 	for _, x := range y {
-	// 		count += x
-	// 	}
-	// }
-
-	fmt.Printf("The matrix is x = %v, y = %v with %v dots\n", max_x+1, max_y+1, 0)
-	// prettyPrintMatrix2D(paper)
 
 	for i := 0; i < len(fold_instructions); i++ {
-		fmt.Println(fold_instructions[i])
 		if fold_instructions[i][0] == "y" {
 
-			along, _ := strconv.Atoi(fold_instructions[i][1])
-			// modifier := 0
-			// if (len(paper) % along) != 0 {
-			// 	for i := range paper {
-			// 		paper[i] = append(paper[i], 0)
-			// 	}
-			// }
-			new := make([][]int, along)
+			along, _ := strconv.Atoi(fold_instructions[i][1]) //where to fold
+			new := make([][]int, along)                       //make a new, blank matrix
 
-			for y := 0; y < along; y++ {
+			for y := 0; y < along; y++ { //fill the blank matrix
 				new[y] = make([]int, len(paper[y]))
 				copy(new[y], paper[y])
 
-				for x := 0; x < len(paper[y]); x++ {
+				for x := 0; x < len(paper[y]); x++ { //fold the paper
 					new[y][x] = (new[y][x] | paper[len(paper)-1-y][x])
 				}
 			}
+
 			paper = new
 
-		} else {
-			along, _ := strconv.Atoi(fold_instructions[i][1])
-
-			// if (len(paper[0]) % along) != 0 {
-			// 	t := make([]int, len(paper[0]))
-			// 	paper = append(paper, t)
-			// }
-			new := make([][]int, len(paper))
-			for y := 0; y < len(paper); y++ {
+		} else { //x
+			along, _ := strconv.Atoi(fold_instructions[i][1]) //where to fold
+			new := make([][]int, len(paper))                  //make a new, correctly sized blank
+			for y := 0; y < len(paper); y++ {                 //fill the blank matrix
 				new[y] = make([]int, along)
 				copy(new[y], paper[y][:along])
 
-				for x := 0; x < along; x++ {
+				for x := 0; x < along; x++ { //fold the paper
 					new[y][x] = (new[y][x] | paper[y][len(paper[y])-1-x])
-					// count += new[y][x]
 				}
 				// break
 			}
-
+			// Make the folded version the real one
 			paper = new
+
+		}
+
+		if i == 0 { //Part 1 answer
+			for _, y := range paper {
+				for _, x := range y {
+					answer_pt1 += x
+				}
+			}
 		}
 
 	}
 	fmt.Printf("The new, folded matrix is x=%v, y=%v\n", len(paper[0]), len(paper))
-	var answer_pt1 int = 0
+
 	// prettyPrintMatrix2D(paper)
 	//Calculate the answer
-	for _, y := range paper {
-		for _, x := range y {
-			answer_pt1 += x
-		}
-	}
+
 	prettyPrintMatrix2D(paper)
 	prettyPrintMatrixLetters(paper)
-	fmt.Println(answer_pt1)
+	fmt.Printf("The Answer to part 1 is: %v \n", answer_pt1)
 
 } //EOF
--- testing/hello.go
+++ testing/hello.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	a := "fold along y=13"
-	y := strings.Split(a, "=")
-	fmt.Println(y)
-	fmt.Println(y[1])
-	fmt.Println(y[0][11:])
+	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8} //len 9, mid = "5"
+	fmt.Println(a[:5])
+	fmt.Println(a[5:])
 
 }
