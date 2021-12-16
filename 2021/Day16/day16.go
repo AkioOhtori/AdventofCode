f153b0fea0d79313c1fee3846c4e5d8d8e13c7f4
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-16 16:43:06 -0600

    Day 16 part 2 cleanish and done enough

--- 2021/Day16/day16.go
+++ 2021/Day16/day16.go
@@ -60,70 +60,59 @@ func checkPacket(packet string, p int) (pp int, output []int) {
 	switch packet_type {
 
 	case LITERAL:
-		// literal value, read 5 bits at a time
-		//need to add bounds checking
+		// literal value, read 5 bits at a time, 1st bit being status, rest being part of a number
 		bits := []string{}
 		for x := 0; x < len(packet); x++ {
 			bits = append(bits, packet[p:p+5])
 			p += 5
-			if bits[x][:1] == "0" {
+			if bits[x][:1] == "0" { //reached the last 5-bit number, stop
 				break
 			}
 		}
-		var o string = ""
+		//Compress the nibbles into an actual number
+		var o string
 		for _, num := range bits {
-			o += string(num[1:])
-			// i, _ := strconv.ParseInt(num[1:], 2, 64)
-			// output = append(output, int(i))
-			fmt.Println(o)
+			o += string(num[1:]) //lop of the status and keep the nibble only
 		}
+		//Convert and return the final literal
 		temp, _ := strconv.ParseInt(o, 2, 64)
 		output = []int{int(temp)}
 		fmt.Printf(" which is a literal of value %v %v.\n", bits, output)
 
-	default:
+	default: //otherwise it is an operator and we need to do some pre-math
 		fmt.Printf(" which is an operator of type %v.\n", packet_type)
 
-		if packet[p:p+1] == "0" {
+		if packet[p:p+1] == "0" { //15-bit number representing the number of bits in the sub-packets
 			p++
 			l := packet[p : p+15]
 			length, _ := strconv.ParseInt(l, 2, 64)
 			p += 15
 			fmt.Printf("We had unknown number of subpackets of length %v\n", length)
-
-			for x := p; p < x+int(length); x = x {
-				// var o []int
-				// p, o = checkPacket(packet, p)
-				// output = append(output, o...)
+			//Calculate the subpackets based on length
+			for x := p; p < x+int(length); _ = x {
 				var o []int
-				var ooo []int
 				fmt.Printf("Going to subpacket %v\n", packet[p:p+11])
-				p, ooo = checkPacket(packet, p)
-				o = ooo
-				copy(o, ooo)
+				p, o = checkPacket(packet, p)
 				output = append(output, o...)
 			}
-		} else {
+		} else { //type == 1; length is a 11-bit number representing the number of sub-packets
 			p++
 			l := packet[p : p+11]
 			subpackets, _ := strconv.ParseInt(l, 2, 64)
 			p += 11
 			fmt.Printf("The length is %vb = %v subpackets\n", l, subpackets)
 
+			//Calculate the subpackets based on number expected
 			for x := 0; x < int(subpackets); x++ {
 				var o []int
-				var ooo []int
-				fmt.Printf("Going to subpacket %v \n", x) //packet[p:p+11])
-				p, ooo = checkPacket(packet, p)
-				o = ooo
-				copy(o, ooo)
+				fmt.Printf("Going to subpacket %v \n", x)
+				p, o = checkPacket(packet, p)
 				output = append(output, o...)
 			}
 		}
-		fmt.Println(output)
 
+		// Now that we have an output, we need to math it based on operator type
 		switch packet_type {
-
 		case 0: //SUM
 			var o int
 			for _, x := range output {
@@ -159,41 +148,31 @@ func checkPacket(packet string, p int) (pp int, output []int) {
 			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, o)
 			output = []int{o}
 		case 5: //greater than
-			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, 555)
-			if len(output) > 2 {
-				fmt.Println(output[999])
-			}
+			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, "T_T")
 			if output[0] > output[1] {
 				output = []int{1}
 			} else {
 				output = []int{0}
 			}
 		case 6: //less than
-			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, 555)
-			if len(output) > 2 {
-				fmt.Println(output[999])
-			}
+			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, "T_T")
 			if output[0] < output[1] {
 				output = []int{1}
 			} else {
 				output = []int{0}
 			}
 		case 7: //equal
-			if len(output) > 2 {
-				fmt.Println(output[999])
-			}
-			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, 555)
+			fmt.Printf("We did a type %v on %v and got %v\n", packet_type, output, "T_T")
 			if output[0] == output[1] {
 				output = []int{1}
 			} else {
 				output = []int{0}
 			}
 		default:
-			fmt.Println("THESUNTHESUNTHESUN")
+			fmt.Println("THESUNTHESUNTHESUN") //should never happen
 		}
-		// fmt.Printf("We did a type %v and came up with %v", packet_type, output)
 	}
-	pp = p
+	pp = p //because it can't be both
 	fmt.Println()
 	return
 }
@@ -209,22 +188,20 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	// var input [][]int
-
-	//Go through the instructions and convert them to slices of slices, [y],[x]
-
+	//load in only input line and convert it from hex to binary
 	for scanner.Scan() {
-		//for p := 0; p < len(a)*4; _++
 		a = scanner.Text()
 		fmt.Println(a, len(a)*4)
 
 		for _, e := range a {
 			bin += Hex2Bin[string(e)] //"stock" conversion suppressed zeros so...
 		}
-		// fmt.Println(bin)
 	}
+
+	//And away we go...
 	p, answer_pt2 := checkPacket(bin, 0)
 	fmt.Println(answer_pt1, p, bin[p:], answer_pt2)
-	fmt.Printf("%T", answer_pt2[0])
-
+	fmt.Printf("The answer to Part 1 is %v and Part 2 is %v", answer_pt1, answer_pt2[0])
 }
+
+//EOF
