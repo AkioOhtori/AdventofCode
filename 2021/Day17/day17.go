7a7ac2164c4df68fd1c07b82b0603d03291e80d9
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-17 14:48:28 -0600

    day 17 part 2 cleaned(ish)

--- 2021/Day17/day17.go
+++ 2021/Day17/day17.go
@@ -18,31 +18,30 @@ func calculateTrajectory(x_vel int, y_vel int, target_x [2]int, target_y [2]int)
 	var x_pos int = 0
 	var y_pos int = 0
 	hit = false
-	// var y_max int = 0
 
 	for i := 0; i < 1000; i++ {
-		// fmt.Printf("Started step %v with position %v, %v and velocity %v, %v \n", i, x_pos, y_pos, x_vel, y_vel)
+		//check to see if we've peaked!
 		if y_pos > y_max {
 			y_max = y_pos
 		}
 
-		x_pos += x_vel
-		y_pos += y_vel
+		x_pos += x_vel //adjust position x
+		y_pos += y_vel //adjust position y
 
-		y_vel--
+		y_vel-- //adjust velocity y
+
+		//if the x velocity is zero, leave it, otherwise follow the rules
 		if x_vel > 0 {
 			x_vel--
 		} else if x_vel < 0 {
 			x_vel++
 		}
 
-		// fmt.Printf("And ended with position %v, %v and velocity %v, %v \n", x_pos, y_pos, x_vel, y_vel)
-
 		if x_pos > target_x[1] || y_pos < target_y[1] { //tells me if we've passed in either x or y
-			// fmt.Println(x_pos, target_x[1], y_pos, target_y[1])
 			y_max = 0
 			return
 		} else if x_pos >= target_x[0] && y_pos <= target_y[0] {
+			//double check we're in the target zone and haven't passed it
 			if x_pos <= target_x[1] && y_pos >= target_y[1] {
 				hit = true
 				return
@@ -50,33 +49,35 @@ func calculateTrajectory(x_vel int, y_vel int, target_x [2]int, target_y [2]int)
 		}
 	}
 	y_max = 0
-	return
+	return //default, should never be reached
 }
 
+//This function check to see if the x, regardless of y, crosses the target zone
 func calcX(x_vel int, target_x [2]int) (int, int) {
 	var x_pos int = 0
+
+	// Arbitrary range we expect to break out of
 	for i := 0; i < 1000; i++ {
-		// fmt.Printf("Starting step %v with vel = %v at pos = %v ", i, x_vel-i, x_pos)
+		//check to see if we've stopped
 		if (x_vel - i) < 0 {
 			x_vel = 0
-			// fmt.Printf("and ending\n")
 			if x_pos > target_x[0] {
 				return (i - 1), 0
 			} else {
 				return -100, -100
 			}
-
-		} else {
+		} else { //if we haven't stopped, decrease the velocity
 			x_pos += (x_vel - i)
 		}
-		// fmt.Printf("and ending with %v, %v\n", x_vel-(i+1), x_pos)
 
+		//check to see if we've overshot the target area
 		if x_pos > target_x[1] {
+			//if we have, that means the last X was (probably) in the target zone
 			return (i - 1), (x_vel + (i - 1))
 		}
 	}
 
-	return 0, 0
+	return -10, -10
 
 }
 
