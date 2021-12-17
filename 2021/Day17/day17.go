baab91d49c30380aea385e9f00f26e060efb4258
mackayeg <46541091+mackayeg@users.noreply.github.com>
2021-12-17 14:41:54 -0600

    day 17 part 2 gross but effective

--- 2021/Day17/day17.go
+++ 2021/Day17/day17.go
@@ -2,23 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-var path = "input.txt" //path to problem input
 const PART int = 2
 
-// sample: target area: x=20..30, y=-10..-5
-
-// Function to handle errors
-func isError(err error) bool {
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	return (err != nil)
-}
-
-func calculateTrajectory(x_vel int, y_vel int, target_x [2]int, target_y [2]int) (y_max int) {
+func calculateTrajectory(x_vel int, y_vel int, target_x [2]int, target_y [2]int) (y_max int, hit bool) {
 	/*
 		1) The probe's x position increases by its x velocity.
 		2) The probe's y position increases by its y velocity.
@@ -29,6 +17,7 @@ func calculateTrajectory(x_vel int, y_vel int, target_x [2]int, target_y [2]int)
 	*/
 	var x_pos int = 0
 	var y_pos int = 0
+	hit = false
 	// var y_max int = 0
 
 	for i := 0; i < 1000; i++ {
@@ -50,11 +39,12 @@ func calculateTrajectory(x_vel int, y_vel int, target_x [2]int, target_y [2]int)
 		// fmt.Printf("And ended with position %v, %v and velocity %v, %v \n", x_pos, y_pos, x_vel, y_vel)
 
 		if x_pos > target_x[1] || y_pos < target_y[1] { //tells me if we've passed in either x or y
-			fmt.Println(x_pos, target_x[1], y_pos, target_y[1])
+			// fmt.Println(x_pos, target_x[1], y_pos, target_y[1])
 			y_max = 0
 			return
 		} else if x_pos >= target_x[0] && y_pos <= target_y[0] {
 			if x_pos <= target_x[1] && y_pos >= target_y[1] {
+				hit = true
 				return
 			}
 		}
@@ -90,69 +80,44 @@ func calcX(x_vel int, target_x [2]int) (int, int) {
 
 }
 
-func calcY(y_vel int, target_y [2]int, n int) bool {
-	tri := ((n * n) + n) / 2
-	yfin := n*y_vel - tri
-	yvelfin := y_vel - tri
-	fmt.Printf("For vel = %v and n = %v, y_fin = %v, yvelfin = %v\n", y_vel, n, yfin, yvelfin)
-	if yfin < target_y[1] {
-		return false
-	}
-	return true
-}
-
 func main() {
-	//Open input file
-	var file, err = os.Open(path)
-	if isError(err) {
-		return
-	}
-	file.Close()
-
-	// //Load all lines in file into a slice of slices
-	// scanner := bufio.NewScanner(file)
-	// scanner.Split(bufio.ScanLines)
-
-	// for scanner.Scan() {
-	// 	temp_str := scanner.Text()
-	// }
-
+	//sample input
 	target_x := [2]int{20, 30}
 	target_y := [2]int{-5, -10}
 
+	//cheating and just hard coding the input
 	target_x = [2]int{57, 116}
 	target_y = [2]int{-148, -198}
 
-	x_range := target_x[1] - target_x[0]
-	y_range := -target_y[1] + target_y[0]
-
-	xxx := make(map[int]int)
 	x_steps := []int{}
-	// validy := []int{}
+	var answer_pt2 int = 0
 
-	fmt.Println(x_range, y_range)
-
-	// fmt.Println(calculateTrajectory(17, -4, target_x, target_y))
-
-	fmt.Println(calcX(9, target_x))
 	for x := 1; x <= target_x[1]; x++ {
+		//steps, velocity
 		s, tv := calcX(x, target_x)
-		if s > 1 && tv == 0 {
-			// xxx = append(xxx, [2]int{x, s})
-			xxx[x] = s
-			x_steps = append(x_steps, x)
+		if PART == 1 {
+			if s > 1 && tv == 0 {
+				x_steps = append(x_steps, x)
+			}
+		} else {
+			if s >= 0 {
+				x_steps = append(x_steps, x)
+			}
 		}
 	}
+
 	ym := 0
 	for _, x := range x_steps {
-		for y := 0; y < 300; y++ {
-			t := calculateTrajectory(x, y, target_x, target_y)
+		for y := (target_y[1]); y < -target_y[1]; y++ {
+			t, h := calculateTrajectory(x, y, target_x, target_y)
 			if t > ym {
 				ym = t
 			}
+			if h {
+				answer_pt2++
+			}
 		}
-
 	}
 
-	fmt.Println(ym)
+	fmt.Printf("The answer to Part 1 is y = %v and Part 2 is %v\n", ym, answer_pt2)
 }
